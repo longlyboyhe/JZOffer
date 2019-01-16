1e396603a2c8e2c753692fd304583c20a01c301f
zhyocean <1125694337@qq.com>
2019-01-16 21:41:03 +0800

    面试题2：实现Singleton模式

--- src/question2/Singleton1.java
+++ src/question2/Singleton1.java
@@ -3,8 +3,19 @@ package question2;
 /**
  * @author: zhangocean
  * @Date: 2019/1/16 21:33
- * Describe:
+ * Describe: 饿汉式单例
  */
 public class Singleton1 {
 
+    //类中创建类对象
+    private static Singleton1 instance = new Singleton1();
+
+    //私有构造函数
+    private Singleton1(){
+    }
+
+    //公共获得单例变量方法
+    public Singleton1 getInstance() {
+        return instance;
+    }
 }
