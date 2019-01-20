7e0cad08b08b0fe0370544595d9164e6901c6d3d
zhyocean <1125694337@qq.com>
2019-01-20 16:33:48 +0800

    面试题14：剪绳子

--- src/question14/Solution1.java
+++ src/question14/Solution1.java
@@ -11,7 +11,7 @@ public class Solution1 {
      * 题目：给你一根长度为n的绳子，请把绳子剪成m段(m、n都是整数，n>1并且m>1)，每段绳子的长度记为k[0],k[1],...,k[m]。
      * 请问k[0]*k[1]*...k[m]可能的最大乘积是多少？例如，当绳子的长度是8时，我们把它剪成长为2,3,3的三段，此时得到的最大乘积是18
      */
-    public static int maxProductAfterCutting_Solution(int length){
+    public static int maxProductAfterCuttingSolution(int length){
         if(length < 2){
             return 0;
         }
@@ -41,6 +41,6 @@ public class Solution1 {
     }
 
     public static void main(String[] args) {
-        System.out.println(maxProductAfterCutting_Solution(8));
+        System.out.println(maxProductAfterCuttingSolution(8));
     }
 }
