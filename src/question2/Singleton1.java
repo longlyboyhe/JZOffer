00aaf16b3efa983073356769770d4ab22c6858de
zhyocean <1125694337@qq.com>
2019-03-03 15:23:50 +0800

    面试题2：设计一个类，我们只能生成该类的一个实例。

--- src/question2/Singleton1.java
+++ src/question2/Singleton1.java
@@ -4,11 +4,12 @@ package question2;
  * @author: zhangocean
  * @Date: 2019/1/16 21:33
  * Describe: 饿汉式单例(静态常量)
+ * 题目：设计一个类，我们只能生成该类的一个实例。
  */
 public class Singleton1 {
 
     //类中创建类对象
-    private static Singleton1 instance = new Singleton1();
+    private static final Singleton1 instance = new Singleton1();
 
     //私有构造函数
     private Singleton1(){
