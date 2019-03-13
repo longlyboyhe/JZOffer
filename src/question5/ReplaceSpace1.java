ff827a05e301bb5b16cdc2501b9572a38e3f2955
zhyocean <1125694337@qq.com>
2019-03-13 14:21:46 +0800

    面试题5：替换空格

--- src/question5/ReplaceSpace1.java
+++ src/question5/ReplaceSpace1.java
@@ -3,7 +3,9 @@ package question5;
 /**
  * @author: zhangocean
  * @Date: 2019/1/18 15:08
- * Describe: Java提供的替换字符串的方法
+ * Describe: 替换空格
+ * 题目：请实现一个函数，把字符串中的每个空格替换成%20.
+ * 例如，输入“We are happy.”，则输出“we%20are%20happy.”。
  */
 public class ReplaceSpace1 {
 
